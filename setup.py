2a30b157c1dec0e1f62fe586f989e28c481334b7
lauramaiocchi <lauradevargasmaiocchi@gmail.com>
2024-04-10 15:43:46 -0300

    application created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="acppred",
+    version="0.0.1",
+    packages=find_packages(),
+    author="Laura de Vargas Maiocchi",
+    author_email="lauradevargasmaiocchi@gmail.com",
+    description="anticancer peptide prediction tool"
+)
